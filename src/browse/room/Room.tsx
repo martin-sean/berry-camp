0bb0cf6027d1d3ed98500bdecfba146f4a88b656
martin-sean <29813268+martin-sean@users.noreply.github.com>
2020-05-04 10:05:29 +1000

    Fixed wrong import from merge

--- src/browse/room/Room.tsx
+++ src/browse/room/Room.tsx
@@ -3,7 +3,7 @@ import Skeleton from '@material-ui/lab/Skeleton';
 
 import { makeStyles, Fade, Theme, Typography, Divider } from '@material-ui/core'
 import { DataTree } from '../../api/Data';
-import { LastRoom } from '../../App';
+import { LastRoom } from '../../BerryCamp';
 
 const imageHost = 'https://cdn.berrycamp.com/file/strawberry-house/screens/'
 
