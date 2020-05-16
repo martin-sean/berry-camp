cfc9ff98594da7dfc6504fdabb447b2f045b78f6
martin-sean <29813268+martin-sean@users.noreply.github.com>
2020-05-16 11:34:31 +1000

    Experimenting with layout

--- src/browse/home/roomclips/RoomClips.tsx
+++ src/browse/home/roomclips/RoomClips.tsx
@@ -10,6 +10,30 @@ const useStyles = makeStyles((theme) => ({
   paper: {
     padding: theme.spacing(3),
   },
+  scrollable: {
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    overflowY: 'auto',
+  },
+  row: {
+    display: 'flex',
+    overflow: 'hidden',
+    height: '100%',
+    width: '100%',
+  },
+  column: {
+    height: '100%',
+    width: '100%',
+    overflow: 'hidden',
+  },
+  columnInner: {
+    height: '100%',
+    width: '100%',
+    overflowY: 'scroll',
+    boxSizing: 'content-box',
+  }
 }));
 
 interface RoomClipsProps {
@@ -23,28 +47,77 @@ export default (props: RoomClipsProps) => {
   const classes = useStyles();
 
   return (
-    <Fade in={ true }>
-      <Grid container spacing={ 3 }>
-        <Grid item xs={ 12 } lg={ 5 }>
+    <div className={ classes.row }>
+      <div className={ classes.column }>
+        <div className={ classes.columnInner }>
           <Paper className={ classes.paper }>
-            <Room
-              data={ props.data }
-              nav={ props.nav }
-              setDocTitle={ props.setDocTitle }
-            />
-          </Paper>
-          {/* Room navigation buttons */}
-          <Nav data={ props.data } nav={ props.nav }/>
-        </Grid>
-        <Grid item xs={ 12 } lg= {7 }>
-          <React.Fragment>
-            <Skeleton animation={ false } height={ 50 }/>
-            <Skeleton animation={ false } height={ 50 }/>
-            <Skeleton animation={ false } height={ 50 }/>
-            <Skeleton animation={ false } height={ 50 }/>
-          </React.Fragment>
-        </Grid>
-      </Grid>
-    </Fade>
+             <Room
+               data={ props.data }
+               nav={ props.nav }
+               setDocTitle={ props.setDocTitle }
+             />
+           </Paper>
+        </div>
+      </div>
+      <div className={ classes.column }>
+        <div className={ classes.columnInner }>
+          <Skeleton animation={ false } height={ 50 }/>
+          <Skeleton animation={ false } height={ 50 }/>
+          <Skeleton animation={ false } height={ 50 }/>
+          <Skeleton animation={ false } height={ 50 }/>
+          <Skeleton animation={ false } height={ 50 }/>
+          <Skeleton animation={ false } height={ 50 }/>
+          <Skeleton animation={ false } height={ 50 }/>
+          <Skeleton animation={ false } height={ 50 }/>
+          <Skeleton animation={ false } height={ 50 }/>
+          <Skeleton animation={ false } height={ 50 }/>
+          <Skeleton animation={ false } height={ 50 }/>
+          <Skeleton animation={ false } height={ 50 }/>
+          <Skeleton animation={ false } height={ 50 }/>
+          <Skeleton animation={ false } height={ 50 }/>
+          <Skeleton animation={ false } height={ 50 }/>
+          <Skeleton animation={ false } height={ 50 }/>
+          <Skeleton animation={ false } height={ 50 }/>
+          <Skeleton animation={ false } height={ 50 }/>
+          <Skeleton animation={ false } height={ 50 }/>
+          <Skeleton animation={ false } height={ 50 }/>
+          <Skeleton animation={ false } height={ 50 }/>
+          <Skeleton animation={ false } height={ 50 }/>
+          <Skeleton animation={ false } height={ 50 }/>
+          <Skeleton animation={ false } height={ 50 }/>
+          <Skeleton animation={ false } height={ 50 }/>
+          <Skeleton animation={ false } height={ 50 }/>
+          <Skeleton animation={ false } height={ 50 }/>
+          <Skeleton animation={ false } height={ 50 }/>
+          <Skeleton animation={ false } height={ 50 }/>
+          <Skeleton animation={ false } height={ 50 }/>
+          <Skeleton animation={ false } height={ 50 }/>
+          <Skeleton animation={ false } height={ 50 }/>
+        </div>
+      </div>
+    </div>
+    // <Fade in={ true }>
+    //   <Grid container spacing={ 3 }>
+    //     <Grid item xs={ 12 } lg={ 5 }>
+    //       <Paper className={ classes.paper }>
+    //         <Room
+    //           data={ props.data }
+    //           nav={ props.nav }
+    //           setDocTitle={ props.setDocTitle }
+    //         />
+    //       </Paper>
+    //       {/* Room navigation buttons */}
+    //       <Nav data={ props.data } nav={ props.nav }/>
+    //     </Grid>
+    //     <Grid item xs={ 12 } lg= { 7 }>
+    //       <div className={ classes.scrollable}>
+    //         <Skeleton animation={ false } height={ 50 }/>
+    //         <Skeleton animation={ false } height={ 50 }/>
+    //         <Skeleton animation={ false } height={ 50 }/>
+    //         <Skeleton animation={ false } height={ 50 }/>
+    //       </div>
+    //     </Grid>
+    //   </Grid>
+    // </Fade>
   );
 }
