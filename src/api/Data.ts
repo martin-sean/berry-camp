ec4564a11d7b491f4e76e43cb9a44d28d74a56f1
martin-sean <29813268+martin-sean@users.noreply.github.com>
2020-06-27 02:18:08 +1000

    oops

---
+++ src/api/Data.ts
@@ -0,0 +1,34 @@
+export interface DataTree {
+  [key: string]: Chapter,
+}
+
+export interface Chapter {
+  chapter_no?: number,
+  name: string,
+  desc: string,
+  official?: boolean,
+  sides: {
+    [key: string]: Side,
+  },
+}
+
+export interface Side {
+  name: string,
+  official?: boolean,
+  checkpoints: {
+    [key: string]: Checkpoint,
+  },
+}
+
+export interface Checkpoint {
+  name: string,
+  abbreviation: string,
+  rooms: {
+    [key: string]: Room,
+  },
+}
+
+export interface Room {
+  name: string,
+  debug_id: string,
+}
