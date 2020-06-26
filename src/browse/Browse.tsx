5cc58530129582e26df9e64cecd6bf56b50b966d
martin-sean <29813268+martin-sean@users.noreply.github.com>
2020-06-27 02:49:26 +1000

    Missed one

--- src/browse/Browse.tsx
+++ src/browse/Browse.tsx
@@ -7,7 +7,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { setData, setNav } from 'redux/actions';
 import fetchJson from 'utils/fetch-json';
 import Navigation from './navigation';
-import { DataTree } from '';
+import { DataTree } from 'api/chapterdata';
 import Breadcrumbs from './breadcrumbs';
 import * as Path from 'pages/paths';
 
